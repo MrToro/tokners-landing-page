969c94b66d5405001bbd5e4da7f40e15e6343f86
Jose Toro <ing.josetoro.2@gmail.com>
2022-11-17 20:19:40 -0500

    Header component

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ const navegationMenu = document.querySelector('.header__navegation-menu');
 
 navegationButton.addEventListener('click', function () {
 	navegationMenu.classList.toggle('active');
+	navegationButton.classList.toggle('active');
 });
 
 // Evitamos que se active el menú en un tamaño de pantalla grande
@@ -11,4 +12,11 @@ const mediaQuery = window.matchMedia('(min-width: 992px)');
 
 mediaQuery.addEventListener('change', function () {
 	navegationMenu.classList.toggle('active', false);
+});
+
+document.addEventListener('keyup', (event) => {
+	if (event.key === 'Escape') {
+		navegationMenu.classList.toggle('active', false);
+		navegationButton.classList.toggle('active', false);
+	}
 });
